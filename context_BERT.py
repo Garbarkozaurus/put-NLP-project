bcdc331d568daf7c8c3254dc022a2d28dc638312
Garbarkozaurus <marcingolski@gmail.com>
2023-06-05 13:43:15 +0200

    Sequential approach, prediction evaluation

--- context_BERT.py
+++ context_BERT.py
@@ -101,6 +101,48 @@ def apply_BERT_to_context(model: BertForMaskedLM, tokenizer: BertTokenizer,
     return list(zip(top_k_words, top_k_values))
 
 
+def apply_sequential_BERT(model: BertForMaskedLM, tokenizer: BertTokenizer,
+                          noisy_words: list[str],
+                          masked_words: list[str], mask_indices: list[int],
+                          context_before: int = 10, context_after: int = 10,
+                          k: int = 10, print_context: bool = False
+                          ) -> list[str]:
+    """Sequentially fills all masks in a list of words. Later predictions
+    use the values that were already predicted for earlier masks.
+    :param lit[srt] noisy_words: list of noisy words (essentially:
+    masked_words, but without masks). Used for selecting best BERT suggestion,
+    based on minimum levenshtein distance
+    :param list[str] masked_words: complete list of masked words within which
+    the context is situated
+    :param list[int] mask_indices: positions of of all masked tokens for BERT
+    to predict within `masked_words`
+    :param int context_before: at most how many tokens before the main mask
+    should be used as context for the prediction. Contexts are truncated to
+    never include other masks.
+    :param int context_after: at most how many tokens after the main mask
+    should be used as context for the prediction.
+    :param int k: how many of the most likely predictions should be taken
+    returned for each mask. The top among them is then selected using
+    :function:`best_word_from_list()`
+
+    :return List of words with masks replaced by appropriate suggestions
+    :rtype: list[str]
+    """
+    restored_words = masked_words[:]
+    for i, mask_index in enumerate(mask_indices):
+        previous_mask = 0 if i == 0 else mask_indices[i-1]
+        next_mask = len(masked_words)-1 if i == len(mask_indices)-1 else \
+            mask_indices[i+1]
+        c_before = min(mask_index-previous_mask-1, context_before)
+        c_after = min(next_mask-mask_index-1, context_after)
+        predictions = apply_BERT_to_context(model, tokenizer, restored_words,
+                                            mask_index, c_before, c_after,
+                                            k, print_context)
+        suggestion = best_word_from_list(noisy_words[mask_index], predictions)
+        restored_words[mask_index] = suggestion
+    return restored_words
+
+
 def best_word_from_list(target_word: str, word_conf_list:
                         list[tuple[str, float]], func: Callable =
                         levenshtein) -> str:
@@ -144,7 +186,7 @@ def present_results(mask_idx: int, word_conf_list: list[tuple[str, float]],
     # they also make everything way more readable
     context_start_idx = max(mask_idx-context_before, 0)
     context_end_idx = min(mask_idx+context_after, len(masked_words)-1)
-    target_word = clean_words[mask_idx]
+    target_word = noisy_words[mask_idx]
     top_suggestion = best_word_from_list(target_word, word_conf_list)
 
     clean_context = " ".join(clean_words[context_start_idx:context_end_idx+1])
@@ -159,21 +201,78 @@ def present_results(mask_idx: int, word_conf_list: list[tuple[str, float]],
     print(f"RESTORED: {noisy_with_suggestion}")
 
 
+def evaluate_BERT_quality(correct_words: list[str], predicted_words: list[str],
+                          noisy_words: list[str]
+                          ) -> tuple[float, float, float]:
+    """Gives basic metrics of prediction quality (accuracy & 2 lev. distances)
+    :param list[str] correct_words: words from the original text, that were
+    noisified and masked - the ones we want the model to predict
+    :param list[str] predicted_words: the words that the model predicted to
+    the replace the corresponding correct words
+    :param list[str] noisy_words: the words that were masked; Levenshtein
+    distance to them used to determine the best choice from model's outputs
+    :return Accuracy of the suggestions (ratio of perfect/all); average
+    levenshtein distance to target clean word; average levenshtein distance
+    to the noisy word
+    :rtype tuple[float, float, float]
+    """
+    perfect_suggestions = 0
+    target_levenshtein_sum = 0
+    noisy_lev_sum = 0
+    n = len(correct_words)
+    for correct, predicted, noisy in zip(correct_words, predicted_words,
+                                         noisy_words):
+        if correct.lower() == predicted:
+            perfect_suggestions += 1
+        target_levenshtein_sum += levenshtein(correct, predicted)
+        noisy_lev_sum += levenshtein(noisy, predicted)
+    return perfect_suggestions/n, target_levenshtein_sum/n, noisy_lev_sum/n
+
+
 if __name__ == "__main__":
     CONTEXT_BEFORE = 100
     CONTEXT_AFTER = 100
     TOP_K = 200
-    clean_words, noisy_words = clean_and_noisy_lists("pastas.txt")
+    clean_words, noisy_words = clean_and_noisy_lists("101-convos.txt")
     mask_idxs = masked_indices(noisy_words)
     masked_words = [mask_word(w) for w in noisy_words]
     model = BertForMaskedLM.from_pretrained('bert-base-uncased',
                                             return_dict=True)
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    win_predictions = []
     for mask_idx in mask_idxs:
         BERT_predictions = apply_BERT_to_context(model, tokenizer,
                                                  masked_words, mask_idx,
                                                  CONTEXT_BEFORE,
                                                  CONTEXT_AFTER, TOP_K)
-        present_results(mask_idx, BERT_predictions, clean_words, noisy_words,
-                        CONTEXT_BEFORE, CONTEXT_AFTER)
-        print()
+        pred = best_word_from_list(noisy_words[mask_idx], BERT_predictions)
+        win_predictions.append(pred)
+        # present_results(mask_idx, BERT_predictions, clean_words, noisy_words,
+                        # CONTEXT_BEFORE, CONTEXT_AFTER)
+        # print()
+
+    seq_restored_words = apply_sequential_BERT(model, tokenizer, noisy_words,
+                                               masked_words, mask_idxs,
+                                               CONTEXT_BEFORE, CONTEXT_AFTER,
+                                               TOP_K)
+    targets = []
+    seq_predictions = []
+    noisy = []
+    print("CLEAN, NOISY, WINDOW, SEQUENTIAL")
+    for i, id in enumerate(mask_idxs):
+        print(clean_words[id], noisy_words[id], win_predictions[i],
+            seq_restored_words[id])
+        targets.append(clean_words[id])
+        seq_predictions.append(seq_restored_words[id])
+        noisy.append(noisy_words[id])
+
+    win_acc, win_avg_lev, win_noisy_lev = \
+        evaluate_BERT_quality(targets, win_predictions, noisy)
+    seq_acc, seq_avg_lev, seq_noisy_lev = \
+        evaluate_BERT_quality(targets, seq_predictions, noisy)
+    print("WINDOW:")
+    print(f"ACCURACY: {win_acc}\tAVERAGE LEVENSHTEIN DISTANCE TO TARGET: "
+          f"{win_avg_lev}\t AVERAGE LEV. DISTANCE TO NOISY: {win_noisy_lev}")
+    print("SEQUENTIAL")
+    print(f"ACCURACY: {seq_acc}\tAVERAGE LEVENSHTEIN DISTANCE TO TARGET: "
+          f"{seq_avg_lev}\t AVERAGE LEV. DISTANCE TO NOISY: {seq_noisy_lev}")
