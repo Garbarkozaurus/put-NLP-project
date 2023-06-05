eeed5364c84f2ee2bc30bf2a767070c55d8a9540
Randoragon <randoragongamedev@gmail.com>
2023-06-05 20:14:17 +0200

    [presentation] Add results section + other tweaks

--- presentation/pyplot/comparison-plots.py
+++ presentation/pyplot/comparison-plots.py
@@ -3,10 +3,10 @@
 import matplotlib.pyplot as plt
 
 # Display names of each compared model
-MODEL_BASE_WORDS = "bw"
-MODEL_BASE_CHARS = "bc"
-MODEL_LARGE_WORDS = "Lw"
-MODEL_LARGE_CHARS = "Lc"
+MODEL_BASE_WIN = "bw"
+MODEL_BASE_SEQ = "bs"
+MODEL_LARGE_WIN = "Lw"
+MODEL_LARGE_SEQ = "Ls"
 
 # Display names of each compared measure
 MEASURE_ACCURACY = "Accuracy"
@@ -17,62 +17,62 @@ MEASURE_LEVENSHTEIN_NOISY = "Levenshtein (noisy)"
 data = {
     "pastas.txt": {
         MEASURE_ACCURACY: {
-            MODEL_BASE_WORDS: 0.172,
-            MODEL_BASE_CHARS: 0.222,
-            MODEL_LARGE_WORDS: 0.170,
-            MODEL_LARGE_CHARS: 0.212,
+            MODEL_BASE_WIN: 0.172,
+            MODEL_BASE_SEQ: 0.222,
+            MODEL_LARGE_WIN: 0.170,
+            MODEL_LARGE_SEQ: 0.212,
         },
         MEASURE_LEVENSHTEIN_CLEAN: {
-            MODEL_BASE_WORDS: 3.280,
-            MODEL_BASE_CHARS: 3.158,
-            MODEL_LARGE_WORDS: 3.239,
-            MODEL_LARGE_CHARS: 3.066,
+            MODEL_BASE_WIN: 3.280,
+            MODEL_BASE_SEQ: 3.158,
+            MODEL_LARGE_WIN: 3.239,
+            MODEL_LARGE_SEQ: 3.066,
         },
         MEASURE_LEVENSHTEIN_NOISY: {
-            MODEL_BASE_WORDS: 3.179,
-            MODEL_BASE_CHARS: 3.153,
-            MODEL_LARGE_WORDS: 3.179,
-            MODEL_LARGE_CHARS: 3.088,
+            MODEL_BASE_WIN: 3.179,
+            MODEL_BASE_SEQ: 3.153,
+            MODEL_LARGE_WIN: 3.179,
+            MODEL_LARGE_SEQ: 3.088,
         },
     },
     "101-convos.txt": {
         MEASURE_ACCURACY: {
-            MODEL_BASE_WORDS: 0.339,
-            MODEL_BASE_CHARS: 0.314,
-            MODEL_LARGE_WORDS: 0.331,
-            MODEL_LARGE_CHARS: 0.308,
+            MODEL_BASE_WIN: 0.339,
+            MODEL_BASE_SEQ: 0.314,
+            MODEL_LARGE_WIN: 0.331,
+            MODEL_LARGE_SEQ: 0.308,
         },
         MEASURE_LEVENSHTEIN_CLEAN: {
-            MODEL_BASE_WORDS: 2.273,
-            MODEL_BASE_CHARS: 2.430,
-            MODEL_LARGE_WORDS: 2.266,
-            MODEL_LARGE_CHARS: 2.439,
+            MODEL_BASE_WIN: 2.273,
+            MODEL_BASE_SEQ: 2.430,
+            MODEL_LARGE_WIN: 2.266,
+            MODEL_LARGE_SEQ: 2.439,
         },
         MEASURE_LEVENSHTEIN_NOISY: {
-            MODEL_BASE_WORDS: 2.427,
-            MODEL_BASE_CHARS: 2.541,
-            MODEL_LARGE_WORDS: 2.449,
-            MODEL_LARGE_CHARS: 2.585,
+            MODEL_BASE_WIN: 2.427,
+            MODEL_BASE_SEQ: 2.541,
+            MODEL_LARGE_WIN: 2.449,
+            MODEL_LARGE_SEQ: 2.585,
         },
     },
     "animal-farm.txt": {
         MEASURE_ACCURACY: {
-            MODEL_BASE_WORDS: 0.229,
-            MODEL_BASE_CHARS: 0.286,
-            MODEL_LARGE_WORDS: 0.251,
-            MODEL_LARGE_CHARS: 0.289,
+            MODEL_BASE_WIN: 0.229,
+            MODEL_BASE_SEQ: 0.286,
+            MODEL_LARGE_WIN: 0.251,
+            MODEL_LARGE_SEQ: 0.289,
         },
         MEASURE_LEVENSHTEIN_CLEAN: {
-            MODEL_BASE_WORDS: 2.761,
-            MODEL_BASE_CHARS: 2.653,
-            MODEL_LARGE_WORDS: 2.692,
-            MODEL_LARGE_CHARS: 2.646,
+            MODEL_BASE_WIN: 2.761,
+            MODEL_BASE_SEQ: 2.653,
+            MODEL_LARGE_WIN: 2.692,
+            MODEL_LARGE_SEQ: 2.646,
         },
         MEASURE_LEVENSHTEIN_NOISY: {
-            MODEL_BASE_WORDS: 2.886,
-            MODEL_BASE_CHARS: 2.843,
-            MODEL_LARGE_WORDS: 2.833,
-            MODEL_LARGE_CHARS: 2.820,
+            MODEL_BASE_WIN: 2.886,
+            MODEL_BASE_SEQ: 2.843,
+            MODEL_LARGE_WIN: 2.833,
+            MODEL_LARGE_SEQ: 2.820,
         },
     }
 }
@@ -81,10 +81,10 @@ data = {
 if __name__ == "__main__":
     fig, axs = plt.subplots(3, 3)
     x = [
-        MODEL_BASE_WORDS,
-        MODEL_BASE_CHARS,
-        MODEL_LARGE_WORDS,
-        MODEL_LARGE_CHARS,
+        MODEL_BASE_WIN,
+        MODEL_BASE_SEQ,
+        MODEL_LARGE_WIN,
+        MODEL_LARGE_SEQ,
     ]
 
     for corpus_name, corpus_data in data.items():
@@ -101,12 +101,14 @@ if __name__ == "__main__":
         measure = MEASURE_LEVENSHTEIN_CLEAN
         ax = plt.subplot(1, 3, 2)
         ax.set_title(measure)
+        plt.ylim(0, 4)
         y = [corpus_data[measure][idx] for idx in x]
         plt.bar(x, y)
 
         measure = MEASURE_LEVENSHTEIN_NOISY
         ax = plt.subplot(1, 3, 3, sharey=ax)
         ax.set_title(measure)
+        plt.ylim(0, 4)
         y = [corpus_data[measure][idx] for idx in x]
         plt.bar(x, y)
 
